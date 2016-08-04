00b865857d9ea740c7845829fdcd237d2a558cbf
xiaobaowei <xiaobaowei@tencent.com>
2016-08-04 10:54:08 +0800

    添加express模板引擎——ejs

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ var express = require('express'),
     users = require('./routes/users');
 var app = express();
 
+var testData = {
+    user: 'xiaobao',
+    message: 'welcome to this system'
+};
+
+//加载模板引擎ejs
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 //静态文件处理
 app.use('/static', express.static('./public'));
 
 //挂载点为/user路由
 app.use('/user', users);
 
-app.get('/', function (req, res) {
-    res.send('hello world');
+app.get('/', function(req, res) {
+    res.render('index', testData);
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('server starts');
 });
